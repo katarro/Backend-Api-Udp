7fbd287a7d5a0971fa226f1523617fdc7c784aef
felipe <pipe.castro.espinoza@gmail.com>
2024-06-30 19:33:05 -0400

    feaure(add): añadio un log para errors.

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-const port = 4001;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // quitará las propiedades no esperadas del DTO
-    transform: true, // transformará los objetos planos en instancias de clases de DTO
-    forbidNonWhitelisted: true, // lanzará una excepción si hay propiedades no esperadas
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
     transformOptions: {
-      enableImplicitConversion: true, // permitirá la conversión implícita de tipos (por ejemplo, convertir un string a un número)
+      enableImplicitConversion: true,
     },
   }));
 
   app.enableCors();
-  
+
+  const port = process.env.PORT || 4001;
   await app.listen(port);
   console.log(`Server is running on: ${port}`);
 
-  bootstrap().catch(error => {
-    console.error('Error during bootstrap:', error);
+  app.use((req, res, next) => {
+    console.log(`Request URL: ${req.url}`);
+    next();
   });
 }
 
-bootstrap();
+bootstrap().catch(error => {
+  console.error('Error during bootstrap:', error);
+});
