d638e5166464ab5784cebcfd1da960ed42ec4bd9
katarro <felipe.castro3@mail.udp.cl>
2024-07-15 18:19:36 -0400

    change(senEmail): se comentó todos los codigo respecto al envio de email con sendrid y aun asi sigue enviandolo.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+// import { InjectCacheManager } from '@nestjs/cache-manager';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
@@ -20,9 +23,18 @@ async function bootstrap() {
   await app.listen(port);
   console.log(`Server is running on: ${port}`);
 
+  // Middleware para registrar las URLs solicitadas
   app.use((req, res, next) => {
     console.log(`Request URL: ${req.url}`);
     next();
+  });
+
+  // Añadir endpoint para limpiar la caché
+  const cacheManager = app.get<Cache>(Cache);
+
+  app.use('/clear-cache', async (req, res) => {
+    await cacheManager.reset();
+    res.send({ message: 'Cache cleared' });
   });
 }
 
